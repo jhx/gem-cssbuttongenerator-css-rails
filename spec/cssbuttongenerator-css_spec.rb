fe2e945bd2ffecc829bb18f01aa3519d6de9eca3
Doc Walker <doc.walker@jameshardie.com>
2013-12-09 14:39:18 -0600

    Add spec for CssbuttongeneratorCss::Rails

--- spec/cssbuttongenerator-css_spec.rb
+++ spec/cssbuttongenerator-css_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
-describe CssbuttongeneratorCss::Rails do
-  it 'is pending'
+describe CssbuttongeneratorCss do
+  it 'provides a VERSION constant' do
+    expect(CssbuttongeneratorCss::Rails::VERSION).to be_kind_of(String)
+  end # it
 end # describe
