a79af60e8013bc0c07cbf88c9b29251bdb76d978
Doc Walker <doc.walker@jameshardie.com>
2013-12-12 21:41:40 -0600

    Added spec for non-blank VERSION

--- spec/cssbuttongenerator-css_spec.rb
+++ spec/cssbuttongenerator-css_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe CssbuttongeneratorCss do
-  it 'provides a VERSION constant' do
-    expect(CssbuttongeneratorCss::Rails::VERSION).to be_kind_of(String)
-  end # it
+  context 'VERSION constant' do
+    it 'is a string' do
+      expect(CssbuttongeneratorCss::Rails::VERSION).to be_kind_of(String)
+    end # it
+    it 'is non-blank' do
+      expect(CssbuttongeneratorCss::Rails::VERSION).to_not be_empty
+    end # it
+  end # context
 end # describe
