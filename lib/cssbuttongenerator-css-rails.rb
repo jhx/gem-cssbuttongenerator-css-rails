fa7fdb3e5c2f2dd8fd7a846213811af040f1abf4
Doc Walker <doc.walker@jameshardie.com>
2014-02-16 16:22:16 -0500

    Fix rubocop Encoding offenses

--- lib/cssbuttongenerator-css-rails.rb
+++ lib/cssbuttongenerator-css-rails.rb
@@ -1,2 +1,3 @@
+# encoding: utf-8
 require 'cssbuttongenerator-css-rails/version'
 require 'cssbuttongenerator-css-rails/engine' if defined?(::Rails)
--- lib/cssbuttongenerator-css-rails/engine.rb
+++ lib/cssbuttongenerator-css-rails/engine.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module CssbuttongeneratorCss
   module Rails
     class Engine < ::Rails::Engine
