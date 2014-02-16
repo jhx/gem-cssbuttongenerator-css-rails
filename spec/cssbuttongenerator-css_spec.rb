82a9da9c32d2f9939e5f3ac268e4fd9b7a55ba93
Doc Walker <doc.walker@jameshardie.com>
2014-02-16 16:15:02 -0500

    Fix rubocop MethodCallParentheses offense

--- spec/cssbuttongenerator-css_spec.rb
+++ spec/cssbuttongenerator-css_spec.rb
@@ -5,7 +5,7 @@ describe CssbuttongeneratorCss::Rails do
   context 'VERSION constant' do
     it 'is a valid SemVer string (semver.org)' do
       version = CssbuttongeneratorCss::Rails::VERSION
-      expect { Semantic::Version.new version }.to_not raise_error()
+      expect { Semantic::Version.new version }.to_not raise_error
     end # it
   end # context
 end # describe
